ae0b0b148856f2efb089e68dad2b3de26b07ca0b
Jamdu <Martinez.josh506@gmail.com>
2021-09-12 23:29:23 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,35 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class App
+{
+    static final double taxRate = 5.5;
+    public static void main( String[] args )
+    {
+
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("What is the order amount? ");
+        double orderAmount = scnr.nextInt();
+
+        System.out.println("What is the state? ");
+        String state = scnr.next();
+
+        System.out.println("In " + state);
+
+        if (state.equals("WI"))
+        {
+            System.out.format("The Subtotal is $%.2f\n", orderAmount);
+
+            double percent = taxRate / 10;
+            System.out.format("The Tax is $%.2f \n", percent);
+
+            orderAmount += percent;
+            System.out.format("The total is $%.2f", orderAmount);
+            System.exit(0);
+        }
+        System.out.println("The total is $" + orderAmount + ".");
+    }
+}
